cb92bf82928b3bea7b71e15786f2cdb0768212f1
matthewdowney18 <matthew_downey@student.uml.edu>
2018-08-10 14:08:20 -0400

    added convolutional layers

--- classifier.py
+++ classifier.py
@@ -3,30 +3,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 from mnist import MNIST
 from matplotlib import pyplot
-
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        #self.conv1 = nn.Conv2d(1, 6, 5)
-        #self.conv1 = nn.Conv2d(6, 16, 5)
-        self.layer1 = nn.Linear(784, 100)
-        self.layer2 = nn.Linear(100, 100)
-        self.layer3 = nn.Linear(100, 10)
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(256, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
         self.criterion = nn.MSELoss()
-        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.05)
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.005)
 
     def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        x = F.relu(self.layer2(x))
-        x = F.relu(self.layer2(x))
-        x = F.relu(self.layer2(x))
-        x = torch.sigmoid(self.layer3(x))
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
-    def backward(self, y_pred, y_train):
-        loss = self.criterion(y_pred, y_train)
+    def backward(self, y_pred, target):
+        loss = self.criterion(y_pred, target)
 
         # Zero the gradients
         self.optimizer.zero_grad()
@@ -37,6 +42,13 @@ class Model(nn.Module):
         # Update the parameters
         self.optimizer.step()
         return loss
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
 
 def make_one_hot(data):
@@ -64,23 +76,43 @@ def plot(total_loss):
 
 
 def main():
-    mn_data = MNIST(
-        '/home/mattd/PycharmProjects/MNIST_classifier/MNIST_dataset/')
+    path = '/home/mattd/PycharmProjects/MNIST_classifier/data'
 
-    x_train, y_train = mn_data.load_training()
-    x_test, y_test = mn_data.load_testing()
-    x_train = torch.tensor(x_train, dtype=torch.float, requires_grad=True)
-    y_train = torch.tensor(y_train, dtype=torch.float, requires_grad=True)
-    x_test = torch.tensor(x_test, dtype=torch.float, requires_grad=True)
-    y_test = torch.tensor(y_test, dtype=torch.float, requires_grad=True)
+    trans = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+    train_set = dset.MNIST(root=path, train=True, transform=trans,
+                           download=True)
+    test_set = dset.MNIST(root=path, train=False, transform=trans,
+                          download=True)
+
+    batch_size = 100
+
+    x_train = train_set.train_data.float()
+    y_train = train_set.train_labels
+    x_test = test_set.test_data.float()
+    y_test = test_set.test_labels
+    x_train = x_train.view(60000, 1, 28, 28)
+    x_test = x_test.view(10000, 1, 28, 28)
+
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_set,
+        batch_size=batch_size,
+        shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_set,
+        batch_size=batch_size,
+        shuffle=False)
 
     classifier = Model()
-
+    print(classifier.parameters())
     y_train_one = make_one_hot(y_train)
 
     total_loss = []
-    for epoch in range(20):
+    for epoch in range(10):
         # Forward Propagation
+
         y_pred = classifier.forward(x_train)
 
         loss = classifier.backward(y_pred, y_train_one)
