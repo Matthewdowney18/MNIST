ea71d25d02519e1cb4ff27c8f2c4a9e403c43369
matthewdowney18 <matthew_downey@student.uml.edu>
2018-08-12 17:58:03 -0400

    added convolutional layers, and changed the loss function

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from mnist import MNIST
+import mnist as MNIST
 from matplotlib import pyplot
 import torchvision
 import torchvision.transforms as transforms
@@ -16,8 +16,8 @@ class Model(nn.Module):
         self.fc1 = nn.Linear(256, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-        self.criterion = nn.MSELoss()
-        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.005)
+        #self.criterion = nn.MSELoss()
+        #self.optimizer = torch.optim.SGD(self.parameters(), lr=0.005)
 
     def forward(self, x):
         # Max pooling over a (2, 2) window
@@ -29,7 +29,7 @@ class Model(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
+    '''
     def backward(self, y_pred, target):
         loss = self.criterion(y_pred, target)
 
@@ -42,7 +42,7 @@ class Model(nn.Module):
         # Update the parameters
         self.optimizer.step()
         return loss
-
+    '''
     def num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
@@ -52,11 +52,11 @@ class Model(nn.Module):
 
 
 def make_one_hot(data):
-    onehot = torch.FloatTensor(len(data), 10).zero_()
+    onehot = torch.LongTensor(len(data), 10).zero_()
     for example, num in enumerate(data):
         onehot.index_put_((torch.tensor(example, dtype=torch.long),
                              torch.tensor(int(num), dtype=torch.long)),
-                            torch.tensor(1, dtype=torch.float))
+                            torch.tensor(1, dtype=torch.long))
     return onehot
 
 
@@ -72,6 +72,9 @@ def get_results(test, actual):
 
 def plot(total_loss):
     pyplot.plot(total_loss)
+    pyplot.title('loss')
+    pyplot.xlabel('epoch')
+    pyplot.ylabel('loss')
     pyplot.show()
 
 
@@ -88,13 +91,6 @@ def main():
 
     batch_size = 100
 
-    x_train = train_set.train_data.float()
-    y_train = train_set.train_labels
-    x_test = test_set.test_data.float()
-    y_test = test_set.test_labels
-    x_train = x_train.view(60000, 1, 28, 28)
-    x_test = x_test.view(10000, 1, 28, 28)
-
 
     train_loader = torch.utils.data.DataLoader(
         dataset=train_set,
@@ -102,28 +98,53 @@ def main():
         shuffle=True)
     test_loader = torch.utils.data.DataLoader(
         dataset=test_set,
-        batch_size=batch_size,
+        batch_size=len(test_set),
         shuffle=False)
 
     classifier = Model()
-    print(classifier.parameters())
-    y_train_one = make_one_hot(y_train)
 
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01,
+                                momentum=0.9)
+
+    running_loss = 0.0
     total_loss = []
-    for epoch in range(10):
-        # Forward Propagation
+    for epoch in range(3):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            input, label = data
+            output = classifier(input)
 
-        y_pred = classifier.forward(x_train)
+            loss = criterion(output, label)
 
-        loss = classifier.backward(y_pred, y_train_one)
 
-        print('epoch: ', epoch, ' loss: ', loss.item())
-        total_loss.append(loss)
+            total_loss.append(float(loss.item()))
+            # Zero the gradients
+            optimizer.zero_grad()
+            # perform a backward pass (backpropagation)
+            loss.backward()
 
+            # Update the parameters
+            optimizer.step()
+
+            #print('epoch: ', epoch, ' loss: ', loss.item()))
+
+            running_loss += loss.item()
+            if i % 100 == 0:# print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
     plot(total_loss)
 
-    test_pred = classifier.forward(x_test)
+    dataiter = iter(test_loader)
+    images, labels = dataiter.next()
+    #images, labels = test_set
 
-    print('accuracy: ' + str(get_results(test_pred, y_test)))
+    outputs = classifier(images)
+    # print images
+
+
+    print('accuracy: ' + str(get_results(outputs,
+        labels)))
 
 main()
